e4d4b6f359f8300cd61acaaa98af37fad71f0e1a
sunhuaibo <hbsun1@126.com>
2015-08-13 11:39:36 +0800

    Update derep.py
    update

--- derep.py
+++ derep.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 __author__ = "Huaibo Sun"
-__email__ = "sunhuaibo@novogene.com"
+__email__ = "huaibo_sun@foxmail.com"
 __version__ = "V1.0"
 __data__ = "2015-08-04"
 import sys
