d98a5751a32f6c458f753d2ce74e57f3761ca6d3
sunhuaibo <hbsun1@126.com>
2017-01-18 18:40:03 +0800

    Add files via upload

--- alpha_diversity.py
+++ alpha_diversity.py
@@ -10,10 +10,12 @@ def Usage():
     exit(__doc__)
 class alpha_diversity():
     def __init__(self,vector):
+        """vector """
         self.vector = np.array(vector)
     def simpson(self):
         """https://en.wikipedia.org/wiki/Diversity_index#Shannon_index"""
-        return sum(self.vector**2)
+        freqs = self.vector / self.vector.sum()
+        return sum(freqs**2)
     def simpson_inverse(self):
         """https://en.wikipedia.org/wiki/Diversity_index#Shannon_index"""
         return 1 / self.simpson()
@@ -22,7 +24,9 @@ class alpha_diversity():
         return 1 - self.simpson()
     def shannon(self):
         """https://en.wikipedia.org/wiki/Diversity_index#Shannon_index"""
-        return -(sum(self.vector * np.log(self.vector)))
+        freqs = self.vector / self.vector.sum()
+        nonzero_freqs = freqs[freqs.nonzero()]
+        return -(sum(nonzero_freqs * np.log(nonzero_freqs)))
     def evenness(self):
         """S = H(obs)/H(max)
         Where H(obs) is the number derived from the Shannon diversity index and H(max) is the maximum possible value of H(obs) (if every species was equally likely)
@@ -36,6 +40,42 @@ class alpha_diversity():
     def clonality(self):
         """ S = 1 − H(obs)/H(max)  ref: DOI: 10.1126/scitranslmed.3010760 """
         return 1 - self.evenness()
+    def singles(self,size=1):
+        """Calculate number of single occurrences (singletons)."""
+        return (self.vector == size).sum()
+    def doubles(self):
+        """Calculate number of double occurrences (doubletons)."""
+        return (self.vector == 2).sum()
+    def osd(self):
+        """Calculate observed OTUs, singles, and doubles."""
+        return self.observed_otus(), self.singles(), self.doubles()
+    def robbins(self):
+        """Calculate Robbins' estimator for the probability of unobserved outcomes."""
+        return self.singles(self.vector) / self.vector.sum()
+    def observed_otus(self):
+        """Calculate the number of distinct OTUs."""
+        return (self.vector != 0).sum()
+    def pielou_e(self):
+        """Calculate Pielou's Evenness index J'."""
+        return self.shannon() / np.log(self.observed_otus())
+    def berger_parker_d(self):
+        """Calculate Berger-Parker dominance.Berger-Parker dominance is defined as the fraction of the sample that belongs to the most abundant OTU."""
+        return self.vector.max() / self.vector.sum()
+    def goods_coverage(self):
+        """Calculate Good's coverage of counts."""
+        f1 = self.singles()
+        N = self.vector.sum()
+        return 1 - (f1 / N)
+    def chao1(self, bias_corrected=True):
+        """Calculate chao1 richness estimator. Uses the bias-corrected version unless `bias_corrected` is ``False`` *and* there are both singletons and doubletons.
+
+        bias_corrected : bool, optional Indicates whether or not to use the bias-corrected version of the equation. If False` *and* there are both singletons and doubletons, the uncorrected version will be used. The biased-corrected version will be used otherwise."""
+        o, s, d = self.osd()
+        if not bias_corrected and s and d:
+            return o + s ** 2 / (d * 2)
+        else:
+            return o + s * (s - 1) / (2 * (d + 1))
+    
 class beta_diversity():
     def __init__(self,vector1,vector2):
         self.vector1 = np.array(vector1)
